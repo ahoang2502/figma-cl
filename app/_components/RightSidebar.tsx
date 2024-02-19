bdacd911f153a63d79a8c012948a7953c2d80fe3
ahoang2502 <athoang252@gmail.com>
2024-02-19 22:27:41 +1100

    color-setting

--- app/_components/RightSidebar.tsx
+++ app/_components/RightSidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import Dimensions from "@/components/settings/Dimensions";
 import Text from "@/components/settings/Text";
@@ -15,6 +15,9 @@ export const RightSidebar = ({
 	isEditingRef,
 	syncShapeInStorage,
 }: RightSidebarProps) => {
+	const colorInputRef = useRef(null);
+	const strokeInputRef = useRef(null);
+
 	const handleInputChange = (property: string, value: string) => {
 		if (!isEditingRef.current) isEditingRef.current = true;
 
@@ -52,8 +55,20 @@ export const RightSidebar = ({
 				fontWeight={elementAttributes.fontWeight}
 				handleInputChange={handleInputChange}
 			/>
-			<Color />
-			<Color />
+			<Color
+				inputRef={colorInputRef}
+				attribute={elementAttributes.fill}
+				attributeType="fill"
+				placeholder="color"
+				handleInputChange={handleInputChange}
+			/>
+			<Color
+				inputRef={strokeInputRef}
+				attribute={elementAttributes.stroke}
+				attributeType="stroke"
+				placeholder="color"
+				handleInputChange={handleInputChange}
+			/>
 			<Export />
 		</section>
 	);
